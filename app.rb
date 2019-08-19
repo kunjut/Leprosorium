59635f320eb8a67258156eecdd45aa2bb9bdb739
Vadim Dimnich <v.dimnich@gmail.com>
2019-08-19 22:05:49 +0300

    add comments

--- app.rb
+++ app.rb
@@ -2,19 +2,26 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
-require 'sqlite3'
+require 'sqlite3'\
 
+# функция инициализации БД
 def init_db
 	@db = SQLite3::Database.new 'Leprosorium.db'
 	@db.results_as_hash = true
 end
 
+# before вызывается каждый раз, кроме configure
 before do
+	# инициализация БД
 	init_db 
 end
 
+# configure вызывается каждый раз, при конфигурации приложения:
+# когда изменился код программы и перезагрузилась страница  
 configure do
+	# инициализация БД
 	init_db
+	# создает таблицу если она не существует
 	@db.execute 'CREATE TABLE IF NOT EXISTS Posts (
 		id           INTEGER PRIMARY KEY AUTOINCREMENT,
 		created_date DATE,
@@ -30,11 +37,16 @@ get '/main' do
 	erb :main
 end
 
+# обработчик get-запроса для /new
+# (браузер получает страницу с сервера)
 get '/new' do
 	erb :new
 end
 
+# обработчик post-запроса для /new
+# (браузер отправляет данные на сервер)
 post '/new' do
+	# получаем переменную из post-запроса
 	@textarea = params[:textarea]
 	erb "<i>You typed:</i> #{@textarea}"
 end
